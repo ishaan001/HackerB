569f4fe2a82ae218e77c9ffb9da34e6076b9940b
ishaan001 <ishaanveerdadhwal@gmail.com>
2018-06-22 05:21:40 +0530

    Mirror image of an array

--- maxArrRec.cpp
+++ maxArrRec.cpp
@@ -1,60 +1,72 @@
 #include<iostream>
 using namespace std;
 
-/*void reverseARR(int* arr,int n)
+bool reverseARR(int* arr,int m,int n)
 {
-    int *arrNew=arr;
+    bool a=1;
     if(n<0)
     {
-        return;
+        return a;
     }
     else
-        cout<<arr[n];
-        cout<<"\n";
-
-        reverseARR(arr,n-1);
-        return;
-
-}*/
-int max_num =-2000;
-int maxARR(int *arr,int n)
-{
-    if(n<0)
     {
-        return max_num;
-    }
-    else{
 
-        if(max_num<arr[n])
+
+        if(arr[n]!=arr[m])
         {
-            max_num=arr[n];
-            maxARR(arr,n-1);
+
+            return 0;
+            reverseARR(arr,m+1,n-1);
         }
-        else
-        {
-            maxARR(arr,n-1);
+        else{
+                reverseARR(arr,m+1,n-1);
         }
+
+
     }
-    return max_num;
-
-
+     return a;
 }
+
 int main()
 {
-    int n;
+    int n,b;
     cin>>n;
     int *arr;
-    int ans=0;
+    int m=0;
+    bool a=0;
+//    int ans=0;
     if(n>0)
     {
         arr=new int[n];
         for(int i=0;i<n;i++)
         {
-            cin>>arr[i];
+
+            cin>>b;
+            if(b<n && b>0)
+            {
+                arr[i]=b;
+            }
+            else
+            {
+                a=0;
+
+            }
+
+
 
         }
-    ans=maxARR(arr,n-1);
-    }
-    cout<<ans;
 
+    if(a)
+        a=reverseARR(arr,m,n-1);
+
+
+
+
+}
+if(a)
+    {
+         cout<<"true";
+    }
+    else
+        cout<<"false";
 }
