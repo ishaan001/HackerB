b8c297bbc792165dade71dd9415bdcf2a31ec83c
ishaan001 <ishaanveerdadhwal@gmail.com>
2018-06-22 05:22:35 +0530

    find the intersection b/w 2 arrays using iterative method

---
+++ intersection.cpp
@@ -0,0 +1,81 @@
+// C code to find intersection when
+// elements may not be distinct
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void Printer(vector<int> vect)
+{
+    int n=vect.size();
+    cout<<"[";
+    for (int i=0; i<n-1; i++)
+       cout << vect[i] << ",";
+    cout<<vect[n-1];
+    cout<<"]";
+}
+// Function to find intersection
+void intersection(int a[], int b[], int n, int m)
+{
+	int i = 0, j = 0;
+	vector<int> vect;
+
+	while (i < n && j < m)
+	{
+
+		if (a[i] > b[j])
+		{
+			j++;
+		}
+
+		else
+		if (b[j] > a[i])
+		{
+			i++;
+		}
+		else
+		{
+			// when both are equal
+			vect.push_back(a[i]);
+			i++;
+			j++;
+		}
+
+	}
+	Printer(vect);
+}
+
+// Driver Code
+int main()
+{
+    int n;
+    cin>>n;
+    int *a,*b;
+    a=new int[n];
+    b=new int[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+
+    }
+     for(int i=0;i<n;i++)
+    {
+        cin>>b[i];
+
+    }
+
+
+	//int a[] = {1,2,1,2,1,3,4};
+	//int b[] = {2,3,2,2,2,1,5};
+
+	//int n = sizeof(a)/sizeof(a[0]);
+	//int m = sizeof(b)/sizeof(b[0]);
+	sort(a,a+n);
+	sort(b,b+n);
+
+	//for(int i=0;i<n;i++)
+    //{
+      //  cout<<a[i];
+
+    //}
+	intersection(a, b, n, n);
+}
