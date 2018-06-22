cf8f5326e45830dc7b600e3c8c079c99ba999189
ishaan001 <ishaanveerdadhwal@gmail.com>
2018-06-22 07:10:04 +0530

    wave printing column wise

---
+++ Matrix.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[10][10];
+    int m,n;
+    cin>>m>>n;
+
+    for(int i=0;i<m;i++)
+    {
+
+        for(int j=0;j<n;j++)
+        {
+            cin>>arr[i][j];
+        }
+
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        if(i%2==0)
+        {
+            for(int j=0;j<m;j++)
+            {
+                cout<<arr[j][i]<<", ";
+            }
+
+        }
+        else{
+            for(int j=m-1;j>=0;j--)
+            {
+                cout<<arr[j][i]<<", ";
+            }
+        }
+
+
+    }
+    cout<<"END";
+}
