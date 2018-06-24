0f8d7c44111e6c28fe927899de0c77c034344674
ishaan001 <ishaanveerdadhwal@gmail.com>
2018-06-24 10:22:26 +0530

    Binary Search Tree ,Prints Pre,Post,In

---
+++ BST.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+
+public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int d)
+    {
+        data=d;
+        left=NULL;
+        right=NULL;
+    }
+};
+Node* buildTree(){
+  int d;
+  cin>>d;
+  if(d==-1)
+  {
+      return NULL;
+  }
+  Node* newNode;
+  newNode=new Node(d);
+  newNode->left=buildTree();
+  newNode->right=buildTree();
+  return newNode;
+
+}
+void PreOrderTree(Node* root)
+{
+        if(root==NULL)
+        {
+            return;
+        }
+        cout<<root->data<<" ";
+        PreOrderTree(root->left);
+        PreOrderTree(root->right);
+}
+void InOrderTree(Node* root)
+{
+        if(root==NULL)
+        {
+            return;
+        }
+
+        InOrderTree(root->left);
+        cout<<root->data<<" ";
+        InOrderTree(root->right);
+}
+void PostOrderTree(Node* root)
+{
+        if(root==NULL)
+        {
+            return;
+        }
+
+        PostOrderTree(root->left);
+        PostOrderTree(root->right);
+        cout<<root->data<<" ";
+}
+
+int main(){
+    Node* root=NULL;
+    root=buildTree();
+    PreOrderTree (root);
+}
