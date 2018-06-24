e5b9b2bb8a73cb52dcc8ce9f04ef2155e9cda3ee
ishaan001 <ishaanveerdadhwal@gmail.com>
2018-06-25 04:38:19 +0530

    second array is mirror of first

---
+++ ScndMirrorFirst.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+
+int checkIfMirror(int *arr1,int *arr2,int n)
+{
+    int m=1;
+    int *arr3=new int[n];
+    for(int i=0;i<n;i++)
+    {
+        arr3[arr1[i]]=i;
+
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(arr3[i]!=arr2[i]){
+            m=0;
+            return m;
+        }
+
+    }
+    return m;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int *arr1=new int[n];
+    int *arr2=new int[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr1[i];
+
+    }
+   // cout<<"array 1 inserted\n";
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr2[i];
+
+    }
+   // cout<<"array 2 inserted\n";
+    int m=checkIfMirror(arr1,arr2,n);
+  //  cout<<"m is"<<m<<"\n";
+    if(m)
+        cout<<"true";
+    else
+        cout<<"false";
+}
+
