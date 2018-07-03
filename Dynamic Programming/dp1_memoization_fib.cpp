0bced177f318231dbf61ca93734086e84acf8022
ishaan001 <ishaanveerdadhwal@gmail.com>
2018-07-03 19:24:34 +0530

    using memoization

--- Dynamic Programming/dp1_memoization_fib.cpp
+++ Dynamic Programming/dp1_memoization_fib.cpp
@@ -8,7 +8,13 @@ int fib(int n){
 	if(n==0) return 0;
 	if(n==1) return 1;
 
+	if(memo[n] != -1)
+	{
+		return memo[n];
+	}
+
 	int ans = fib(n - 1) + fib(n - 2);
+	memo[n] = ans;
 	return ans;
 	
 }
