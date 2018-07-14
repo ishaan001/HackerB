d41e292ff34a91d8b38cec55fe5f018fd3cf780e
ishaan001 <ishaanveerdadhwal@gmail.com>
2018-07-14 17:04:43 +0530

    optimised to N from N^3

--- array/MaxsSubArraySum.cpp
+++ array/MaxsSubArraySum.cpp
@@ -5,22 +5,30 @@ int main(){
 
 	int n; cin>>n;
 	int a[1000];
+	int cumSum[1000] = {0}; //cumulative sum will be calculated to solve in better 
+							//time complexity as currently it is N^3 cus of 3 nested loops 
 	int currentSum = 0, maxSum = 0;
 	int left = -1,right = -1;
-	for (int i = 0; i < n; ++i)
+
+	cin >> a[0];
+	cumSum[0] = a[0];
+	for (int i = 1; i < n; ++i)
 	{
 		/* code */
 		cin >> a[i];
+		cumSum[i] = a[i] + cumSum[i-1];
 	}
 
 	for(int i = 0 ; i < n ; i++){
 		
 		for(int j = i ; j < n ;j++){
 			currentSum = 0;
-			for(int k = i ; k <= j ; k++){
+			/*for(int k = i ; k <= j ; k++){
 				currentSum+=a[k];
 
-			}
+			}*/
+
+			currentSum = cumSum[j] - cumSum[i-1];
 			if(currentSum > maxSum) {
 				maxSum = currentSum;
 				left = i; //left index of the elements which makes tha max sub array
