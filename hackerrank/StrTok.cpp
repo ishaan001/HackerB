c39d900dd7cf70f7c50e37c153ddbb1d9899d2c3
ishaan001 <ishaanveerdadhwal@gmail.com>
2018-07-26 23:59:35 +0530

    changes made split function

--- hackerrank/StrTok.cpp
+++ hackerrank/StrTok.cpp
@@ -6,14 +6,24 @@ using namespace std;
  
 int main() 
 {
-	vector<string> v;
+	vector<string> v,v1;
     char input[100] = "A bird came down the walk";
     char *token = std::strtok(input, " ");
     while (token != NULL) {
         v.push_back(token);
         token = std::strtok(NULL, " ");
     }
+    char input1[100] = "ishaan";
+    char *token1 = std::strtok(input1,"a");
+    while (token1 != NULL) {
+        v1.push_back(token1);
+        token1 = std::strtok(NULL, "a");
+    }
     for(int i = 0; i<v.size();i++){
     	cout << v[i] << "\n";
+    }
+    cout<<endl;
+    for(int i = 0; i<v1.size();i++){
+    	cout << v1[i] << "\n";
     }
 }
