b83f3032ac6f4c9ee54ec27a37baa19f5691ecf9
ishaan001 <ishaanveerdadhwal@gmail.com>
2018-07-26 23:35:28 +0530

    anagram problem

---
+++ hackerrank/String.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <string.h>
+using namespace std;
+
+int main(){
+string token, mystring("scott>=tiger");
+while(token != mystring){
+  token = mystring.substr(0,mystring.find_first_of(">="));
+  mystring = mystring.substr(mystring.find_first_of(">=") + 1);
+  //cout << mystring<< endl;
+  cout << token.c_str();
+}
+cout << token <<endl;
+cout << mystring;
+cout << mystring.split(">=");
+}
