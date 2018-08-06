5ef08cacc6dd7fd3a0db52b08a1a5ca71fa2189b
ishaan001 <ishaanveerdadhwal@gmail.com>
2018-08-06 21:29:03 +0530

    introduction to getter and setter

--- OOPs/classObject.cpp
+++ OOPs/classObject.cpp
@@ -3,13 +3,19 @@ using namespace std;
 
 
 class Car{
+private :
+	int price;
+
 public:
 	int model_No;
-	int price;
+	
 
 	char Name[20];
 	void start(){
 		cout << "GRrrrrrr....." << endl;
+	}
+	void setPrice(int p){
+		price = p;
 	}
 };
 int main()
@@ -19,12 +25,15 @@ int main()
 	cout << sizeof(Car) << endl;
 
 	c.model_No = 1198;
-	c.price = 250000;
+//	c.price = 250000; this can't be done as price is the private datamember
+	c.setPrice(25000);
 	c.Name[0] = 'A';
 	c.Name[1] = 'U';
 	c.Name[2] = 'D';
 	c.Name[3] = 'I';
 	c.Name[4] = '\0';
 
-	c.start();
+	c.start(); 
+	//cout << c.price << endl;
+	cout << c.Name << endl;
 }
