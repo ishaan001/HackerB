7362038857424e5e7c3561da0914170c0568fa2a
ishaan001 <ishaanveerdadhwal@gmail.com>
2018-08-10 11:35:03 +0530

    pointers concept

---
+++ OOPs/pointers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+	int arr[] = {11,2,3,4,5,6,10};
+	char c = 'a';
+    /*int *ptr = arr;
+    int **ptr1 = nullptr;
+    ptr1 = &ptr;
+	cout << *ptr1 <<endl;
+	*/
+
+	char& a = c;
+	cout << a <<endl;
+
+	return 0;
+}
