a44fd490dfbd3c26a3c540e711edd38a61203235
ishaan001 <ishaanveerdadhwal@gmail.com>
2018-08-15 10:46:12 +0530

    sanjay's problem hacker blocks

---
+++ hackerrank/sanjaysProb.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+
+bool Mycompare(pair<string, int> p1, pair<string, int> p2){
+    if(p1.second == p2.second){
+        return p1.first < p2.first;
+    }
+    return p1.second > p2.second;
+}
+
+int main(){
+    int n; // number of entries
+    int minSal ; // minimum salary
+    pair<string, int> emp[1000]; //pair which will take name and salary
+    string name ;
+    int salary;
+
+    cin >> minSal;
+    cin >> n;
+    for(int i = 0; i < n ;i++){
+        cin >> name >> salary;
+        emp[i].first = name ;
+        emp[i].second = salary ;
+    }
+
+    sort(emp, emp+n, Mycompare);
+
+    for(int i = 0; i < n ; i++){
+        if(emp[i].second > minSal)
+        cout << emp[i].first << " " << emp[i].second << endl;
+    }
+
+
+}
