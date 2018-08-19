e43a5e7a104ba8ac08f93be060b2e1cbe1c3ba07
ishaan001 <ishaanveerdadhwal@gmail.com>
2018-08-20 03:09:39 +0530

    new features added onto linked list

--- linkedList/LinkedList1.cpp
+++ linkedList/LinkedList1.cpp
@@ -23,12 +23,59 @@ public:
 */
 
 // functional aproach for CRUD operations
+//passing the pointer variable via refernce
 void insertAtHead (Node *&head, int data){
 	Node * n = new Node(data);
 	n -> next = head;
 	head = n;
 }
+void insertAtEnd (Node *&head, int data){
+	if(head == NULL){
+		head = new Node(data);
+		return;
+	}
+	//de * n = new Node(data);
+	Node * ptr = head;
+	while(ptr -> next != NULL){
+		ptr = ptr ->next;
+	}
+	ptr -> next = new Node(data);
+	return;
+}
+int length(Node* head){
+	Node *ptr = head;
+	int len = 0;
+	while(head != NULL ){
+//		cout << ptr -> data << " -> " ;
+		head = head->next;
+		len += 1; 
+	}
+	return len;
+}
+void insertAtMiddle(Node* &head, int data, int p){
+	if(head == NULL || p == 0)	{
+		insertAtHead(head, data);
 
+	}
+	else if(p > length(head)){
+		insertAtEnd(head, data);
+	}
+	else
+	{
+		// insert in the middle
+		int jump =1 ;
+		Node * temp =head;
+		while(jump <= p-1){
+			temp = temp->next;
+			jump += 1;
+		}
+		Node *n = new Node(data);
+		n -> next = temp -> next;
+		temp -> next = n;
+
+	}
+
+}
 void printList (Node *head){
 	Node *ptr = head;
 	while(ptr){
@@ -44,9 +91,10 @@ int main()
 	Node *head =NULL;
 	insertAtHead(head, 5);
 	insertAtHead(head, 4);
-	insertAtHead(head, 1);
-	insertAtHead(head, 7);
-	insertAtHead(head, 9);
+	//printList(head);
+	insertAtEnd(head, 6);
+	//printList(head);
+	insertAtMiddle(head, 2, 1);
 	printList(head);
 	return 0;
 }
